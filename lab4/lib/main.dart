eb015429ef173ba5d73d4daf6cdf8677216963cd
PolyaSt01 <tntpolya2010@gmail.com>
2023-03-17 12:31:04 +0300

    added buttons

--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -30,49 +30,124 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var _descriptionOfTheHostel = Column(
-    mainAxisSize: MainAxisSize.max,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    textBaseline: TextBaseline.alphabetic,
-    // textBaseline: TextBaseline.alphabetic,
-    children: const <Widget>[
-      Text(
-        'Общежитие №20',
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,),
-        textAlign: TextAlign.left,
-        textDirection: TextDirection.ltr,
+  var _titleAndButton = Container(
+    padding: const EdgeInsets.all(28),
+    child: Row(
+      children:
+      [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Общежитие № 20',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              Text(
+                'Краснодар, ул. Калинина, 13',
+                style: TextStyle(color: Colors.grey,fontSize: 16,),
+              ),
+            ],
+          ),
+        ),
+        Icon(
+          Icons.favorite,
+          color: Colors.red,
+        ),
+        const Text('27',
+        style: TextStyle(fontSize: 17),),
+      ],
+    ),
+  );
+
+  var _buttonCall = Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        Icons.phone,
+        color: Colors.green,
       ),
 
-      Text(
-        'Краснодар, ул. Калинина, 13',
-        style: TextStyle(fontSize: 16, color: Colors.grey,),
-        textAlign: TextAlign.left,
-        textDirection: TextDirection.ltr,
-      ),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          'Позвонить',
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400,color: Colors.green),
+        ),
+      )
     ],
   );
 
-  var _buttonHeart = Column(
-    children: const <Widget>[
-      IconButton(
-        icon: const Icon(Icons.favorite, color: Colors.red,),
-        onPressed: null,
+  var _buttonPath = Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        Icons.navigation,
+        color: Colors.green,
       ),
-      Text('27',
-      style: TextStyle(fontSize: 16),
-      ),
+
+
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          'Маршрут',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400,color: Colors.green),
+        ),
+      )
     ],
   );
 
+  var _buttonShare = Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        Icons.share,
+        color: Colors.green,
+      ),
+
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          'Поделиться',
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400,color: Colors.green),
+        ),
+      )
+    ],
+  );
+
+  var _descriptionOfTheHostel = Padding(
+    padding: EdgeInsets.all(28),
+    child: Text(
+      'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
+      style: TextStyle(fontSize: 16, color: Colors.black,),
+    ),
+  );
+
+
+  var _descriptionOfTheHostel = Padding(
+    padding: EdgeInsets.all(28),
+    child: Text(
+      'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
+      style: TextStyle(fontSize: 16, color: Colors.black,),
+    ),
+  );
 
 
   @override
   Widget build(BuildContext context) {
 
-    var _descriptionButton = Row(
-      children: <Widget>[
-        _descriptionOfTheHostel,
-        _buttonHeart,
+    var _buttons = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+          _buttonCall,
+          _buttonPath,
+          _buttonShare,
       ],
     );
 
@@ -82,159 +157,13 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
 
     body: ListView(
-        // crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Image.asset('assets/images/15.jpg'),
-
-
-          _descriptionButton,
-
-          // descriptionOfTheHostel,
-          // buttonHeart,
-
-          const Text(
-          'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитияхуниверситета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов инаглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
-          style: TextStyle(fontSize: 16, color: Colors.grey,),
-        ),
-
+          _titleAndButton,
+          _buttons,
+          _descriptionOfTheHostel,
         ],
       ),
-
-
-
-
-      // onPressed: () {
-      //   setState(() {
-      //     _volume += 10;
-      //   });
-      // },
-      // Text('Volume : $_volume'),
-
-
-      // body: Container(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.start,
-      //     children: [
-      //       Image.asset('assets/images/15.jpg'),
-      //
-      //       Column(
-      //         children: [
-      //           descriptionOfTheHostel,
-      //
-      //           Icon(Icons.favorite, color: Colors.red,),
-      //           const Text('27'),
-      //
-      //         ],
-      //       ),
-      //
-      //
-      //       // const Text(
-      //       //   'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитияхуниверситета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов инаглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
-      //       //   style: TextStyle(fontSize: 20, color: Colors.grey,),
-      //       // ),
-      //     ],
-      //   ),
-      // ),
-
-
-
-
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-
     );
   }
 }
-
-
-
-
-// IconButton(
-// icon: const Icon(Icons.volume_up),
-// tooltip: 'Increase volume by 10',
-// onPressed: () {
-// setState(() {
-// _volume += 10;
-// });
-// },
-// ),
-// Text('Volume : $_volume'),
-
-
-// body: Container(
-// child: Column(
-// children: [
-// Image.asset('assets/images/15.jpg'),
-//
-// descriptionOfTheHostel,
-//
-// const Icon(Icons.favorite, color: Colors.red,),
-// const Text('27'),
-// ],
-//
-//
-//
-// // const Text(
-// //   'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитияхуниверситета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов инаглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
-// //   style: TextStyle(fontSize: 20, color: Colors.grey,),
-// // ),
-// ),
-//
-//
-//
-// // body: Container(
-// //   child: Column(
-// //     mainAxisAlignment: MainAxisAlignment.start,
-// //     children: [
-// //       Image.asset('assets/images/15.jpg'),
-// //
-// //       Column(
-// //         children: [
-// //           descriptionOfTheHostel,
-// //
-// //           Icon(Icons.favorite, color: Colors.red,),
-// //           const Text('27'),
-// //
-// //         ],
-// //       ),
-// //
-// //
-// //       // const Text(
-// //       //   'Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитияхуниверситета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов инаглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию.',
-// //       //   style: TextStyle(fontSize: 20, color: Colors.grey,),
-// //       // ),
-// //     ],
-// //   ),
-// // ),
-//
-//
-//
-//
-// // floatingActionButton: FloatingActionButton(
-// //   onPressed: _incrementCounter,
-// //   tooltip: 'Increment',
-// //   child: const Icon(Icons.add),
-// // ), // This trailing comma makes auto-formatting nicer for build methods.
-// ),
-
-
-
-// body: ListView(
-// children: [
-// Image.asset('assets/images/15.jpg'),
-// const Text(
-// 'Общежитие №20',
-// style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16,),
-// textAlign: TextAlign.left,
-// ),
-//
-// const Text(
-// 'Краснодар, ул. Калинина, 13',
-// style: TextStyle(fontSize: 16, color: Colors.grey,),
-// textAlign: TextAlign.left,
-// ),
-// ],
-// ),
