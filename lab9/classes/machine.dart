92951f92237cf4f6a92e527f1500341cddef2644
PolyaSt01 <tntpolya2010@gmail.com>
2023-04-24 14:59:59 +0300

    added the classes folder

---
+++ lab9/classes/machine.dart
@@ -0,0 +1,13 @@
+class Machine{
+  late var coffeeBeans;
+  late var milk;
+  late var water;
+  late var cash;
+
+  // get getResource = >
+  // set setResource() =>
+
+
+  Machine(this.coffeeBeans, this.milk, this.water, this.cash);
+
+}
